10332ae284c593b044062475a0cf227f67c32de3
jxlwqq <jxlwqq@gmail.com>
2021-11-18 11:20:01 +0800

    grpc graceful stop

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	v1 "github.com/jxlwqq/todo/api/todo/v1"
 	"github.com/jxlwqq/todo/internal/config"
 	flag "github.com/spf13/pflag"
@@ -10,6 +12,10 @@ import (
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var flagConfig = flag.String("config", "./configs/config.yaml", "path to the config file")
@@ -25,17 +31,34 @@ func main() {
 	todoServer, _ := InitTodoServer(conf.DSN)
 	healthServer := health.NewServer()
 
-	server := grpc.NewServer()
-	reflection.Register(server)
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
 
-	v1.RegisterTodoServer(server, todoServer)
-	grpc_health_v1.RegisterHealthServer(server, healthServer)
+	v1.RegisterTodoServer(grpcServer, todoServer)
+	grpc_health_v1.RegisterHealthServer(grpcServer, healthServer)
 
 	lis, err := net.Listen("tcp", conf.GRPC.Port)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err = server.Serve(lis); err != nil {
-		log.Fatal(err)
+
+	go func() {
+		if err = grpcServer.Serve(lis); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	fmt.Println("Shutting down the grpcServer...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	grpcServer.GracefulStop()
+	select {
+	case <-ctx.Done():
+		close(ch)
 	}
+	fmt.Println("Graceful Shutdown end ")
 }
